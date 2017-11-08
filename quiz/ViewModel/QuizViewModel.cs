cc3b487652e042bcea254138d0d15ca803d1c254
RapidRaid <dzdesigning@gmail.com>
2017-11-08 21:52:23 +0100

    restructured QuizViewModel. Not fully implemented INofifyPropertyChangedEvents. Extended Init()

--- quiz/ViewModel/QuizViewModel.cs
+++ quiz/ViewModel/QuizViewModel.cs
@@ -6,25 +6,42 @@ using System.Text;
 using System.Threading.Tasks;
 using quiz.Command;
 using quiz.Model;
+using System.Windows.Forms;
+
 namespace quiz.ViewModel
 {
     public class QuizViewModel : INotifyPropertyChanged
     {
-        #region properties
-        public event PropertyChangedEventHandler PropertyChanged;
+        #region constructor
 
         public QuizViewModel()
         {
             Init();
         }
+        #endregion
 
+        #region properties
+
+        public event PropertyChangedEventHandler PropertyChanged;
         public Dictionary<int, Frage> DictFragen { get; private set; }
         public CommandButtonOne CommandButtonOne { get; set; }
         public CommandButtonTwo CommandButtonTwo { get; set; }
         public CommandButtonThree CommandButtonThree { get; set; }
         public CommandButtonFour CommandButtonFour { get; set; }
         public QuizManager QuizManager { get; set; }
-        public Frage AktiveFrage { get; set; }
+
+        private Frage aktiveFrage;
+
+        public Frage GetAktiveFrage()
+        {
+            return aktiveFrage;
+        }
+
+        public void SetAktiveFrage(Frage value)
+        {
+            aktiveFrage = value;
+            //OnPropertyChanged("FrageAenderung");
+        }
 
         #endregion
 
@@ -32,21 +49,37 @@ namespace quiz.ViewModel
 
         private void Init()
         {
+            //init new objs
+
+            CommandButtonOne = new CommandButtonOne(this);
+            CommandButtonTwo = new CommandButtonTwo(this);
+            CommandButtonThree = new CommandButtonThree(this);
+            CommandButtonFour = new CommandButtonFour(this);
             QuizManager = new QuizManager();
 
+            //link eventhandler
+           
+
+            
             DictFragen = QuizManager.ErstelleFragen();
 
             // Erste aktive Frage ist eine zufällige Frage
             Random random = new Random();
             int randomFrage = random.Next(DictFragen.Count);
 
-            AktiveFrage = DictFragen[randomFrage];
+            SetAktiveFrage(DictFragen[randomFrage]);
         }
-        protected virtual void OnPropertyChanged()
+
+        // Used for updating the GUI, once a new question is pulled
+    
+        protected virtual void OnPropertyChanged(string frageaenderung)
         {
-
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(frageaenderung));
+            //aktionen zum ausführen hier einfügen
+            MessageBox.Show("debug");
         }
-
+        
+ 
 
         #endregion
 
